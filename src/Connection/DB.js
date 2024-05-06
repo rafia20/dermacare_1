b958a1357873dda65686a31e6a80c0512b87f724
rafia20 <rafiasajid419@gmail.com>
2024-05-06 19:31:51 +0500

    Updated sign up ,login and profile data

--- src/Connection/DB.js
+++ src/Connection/DB.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you needed
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getDatabase } from "firebase/database";
